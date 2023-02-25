e51211c4468d19b24c34b4827b5e5ff313aa8e56
DevvSakib <devvsakib@gmail.com>
2023-02-25 17:05:17 +0600

    api folder added

---
+++ api-practice-challenge/api-1-data.js
@@ -0,0 +1,30 @@
+const person = {
+  found: 2,
+  message: "Found 2 persons",
+  result: [
+    {
+      name: {
+        common: "John",
+        fullName: ["John", "Doe"]
+      },
+      age: 32,
+      isMale: false,
+      address: {
+        street: "13/A St Joseph",
+        house: 10,
+      },
+    },
+    {
+      name: {
+        common: "Humayoun",
+        fullName: ["Humayoun", "Kabir"]
+      },
+      age: 33,
+      isMale: false,
+      address: {
+        street: "13/A St Lucia",
+        house: 11,
+      },
+    },
+  ]
+};
