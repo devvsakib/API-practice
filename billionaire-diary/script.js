96e48b83287d7f1ffc61b712c5b634b9c95e0e16
DevvSakib <devvsakib@gmail.com>
2023-03-01 21:13:05 +0600

    modal details complete

--- billionaire-diary/script.js
+++ billionaire-diary/script.js
@@ -66,7 +66,25 @@ const showDetails = (rank) => {
             console.log(person);
             document.querySelector("#detailsCard").children[0].children[0].innerText = person.person.name
             document.querySelector("#detailsCard").children[0].children[2].innerText = person.bio ? person.bio : "No biography available"
-            document.querySelector("#detailsCard").children[1].children[0].children[0].src = `${person.squareImage.slice(0, 6) === "https:" ? person.squareImage
-            : "https:" + person.squareImage.slice(1)}`
+            document.querySelector("#detailsCard").children[1].children[0].children[0].src = `${person.squareImage.slice(0, 6) === "https:" ? person.squareImage : "https:" + person.squareImage.slice(1)}`
+            document.querySelector("#detailsCard").children[1].children[0].children[1].children[1].innerText = person.source
+            document.querySelector("#generalInfo").innerHTML = `
+            <p><b>Citizenship: </b>${person.countryOfCitizenship}</p>
+            <p><b>State: </b>${person.state ? person.state : "No State Found"}</p>
+            <p><b>City: </b>${person.city}</p>
+            <p><b>Birthday: </b>${bday(person.birthDate)}</p>
+            <p><b>Gender: </b>${person.gender}</p>
+            `
+            document.querySelector("#wealthInfo").innerHTML = `
+            <p><b>Exchange: </b>${person.financialAssets[0].exchange}</p>
+            <p><b>Ticker: </b>${person.financialAssets[0].ticker}</p>
+            <p><b>Total Shares: </b>${person.financialAssets[0].numberOfShares}</p>
+            <p><b>Share Price: </b>${person.financialAssets[0].sharePrice.toFixed(2)}</p>
+            `
         })
+}
+
+const bday = day => {
+    let date = new Date(day)
+    return date.toDateString()
 }
